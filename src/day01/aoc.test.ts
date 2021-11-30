8fedc14c5467987992c28c28f18e914244455a7a
Erik Johnsson <erik@erikjohnsson.se>
2021-11-30 17:03:05 +0100

    Fix parse test

--- src/day01/aoc.test.ts
+++ src/day01/aoc.test.ts
@@ -1,9 +1,12 @@
-import { getSolutionPart1, getSolutionPart2 } from './aoc';
+import { getSolutionPart1, getSolutionPart2, parseInput } from './aoc';
+
+const testInput1 = `1721\n979\n366\n299\n675\n1456`;
+const testInput2 = `1721\n979\n366\n299\n675\n1456`;
 
 test('Test part 1', () => {
-    expect(getSolutionPart1([1721, 979, 366, 299, 675, 1456])).toBe(514579);
+    expect(getSolutionPart1(parseInput(testInput1))).toBe(514579);
 });
 
 test('Test part 2', () => {
-    expect(getSolutionPart2([1721, 979, 366, 299, 675, 1456])).toBe(241861950);
+    expect(getSolutionPart2(parseInput(testInput2))).toBe(241861950);
 });
