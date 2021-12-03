febcc510cdac4c84d5e79bece9fd95bf89a5ba11
Erik Johnsson <erik@erikjohnsson.se>
2021-12-03 11:36:27 +0100

    almost day 3

---
+++ src/day03/aoc.test.ts
@@ -0,0 +1,23 @@
+import { getSolutionPart1, getSolutionPart2, parseInput } from './aoc';
+
+const testInput1 = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+const testInput2 = testInput1;
+
+test('Test part 1', () => {
+    expect(getSolutionPart1(parseInput(testInput1))).toBe(198);
+});
+
+test('Test part 2', () => {
+    expect(getSolutionPart2(parseInput(testInput2))).toBe(5);
+});
---
+++ src/day03/benchmark.ts
@@ -0,0 +1,28 @@
+import b from 'benny';
+import { getInput } from '../common/get-input';
+import { DAY, getSolutionPart1, getSolutionPart2, parseInput } from './aoc';
+b.suite(
+    `AOC Day ${DAY}`,
+    b.add('Parsing', async () => {
+        const input = await getInput(DAY);
+        return () => {
+            parseInput(input);
+        };
+    }),
+    b.add('Part 1', async () => {
+        const input = await getInput(DAY);
+        return () => {
+            const pInput = parseInput(input);
+            getSolutionPart1(pInput);
+        };
+    }),
+    b.add('Part 2', async () => {
+        const input = await getInput(DAY);
+        return () => {
+            const pInput = parseInput(input);
+            getSolutionPart2(pInput);
+        };
+    }),
+    b.cycle(),
+    b.complete()
+);
